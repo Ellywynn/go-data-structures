594fd14bbd256ac29ab7a159c0248b11942982a6
ellywynn <canttrymybest@gmail.com>
2021-12-21 04:49:38 +0300

    added stack size

--- stack/stack.go
+++ stack/stack.go
@@ -7,37 +7,41 @@ var (
 )
 
 type Stack struct {
-	Size   int
+	size   int
 	values []int
 }
 
 func New() *Stack {
 	return &Stack{
-		Size:   0,
+		size:   0,
 		values: make([]int, 0, 5),
 	}
 }
 
 func (s *Stack) Push(value int) {
 	s.values = append(s.values, value)
-	s.Size++
+	s.size++
 }
 
 func (s *Stack) Pop() (int, error) {
-	if s.Size == 0 {
+	if s.size == 0 {
 		return 0, errStackEmpty
 	}
 
-	v := s.values[s.Size-1]
-	s.values = s.values[:s.Size-1]
-	s.Size--
+	v := s.values[s.size-1]
+	s.values = s.values[:s.size-1]
+	s.size--
 	return v, nil
 }
 
 func (s *Stack) Peek() (int, error) {
-	if s.Size == 0 {
+	if s.size == 0 {
 		return 0, errStackEmpty
 	}
 
-	return s.values[s.Size-1], nil
+	return s.values[s.size-1], nil
+}
+
+func (s *Stack) Size() int {
+	return s.size
 }
