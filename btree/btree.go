b1bc1a9151aa94184b820941262fd6d84e3fc75d
ellywynn <canttrymybest@gmail.com>
2021-12-22 00:20:59 +0300

    rewrited btree. now uses only Node struct

--- btree/btree.go
+++ btree/btree.go
@@ -1,78 +1,44 @@
 package btree
 
-type node struct {
-	Left  *node
-	Right *node
+type Node struct {
+	Left  *Node
+	Right *Node
 	Value int
 }
 
-func newNode(value int) *node {
-	return &node{
-		Left:  nil,
-		Right: nil,
-		Value: value,
-	}
-}
-
-type BTree struct {
-	root *node
-}
-
 // Creates a new Binary Tree instance
-func New() *BTree {
-	return &BTree{
-		root: nil,
-	}
+func New(value int) *Node {
+	return &Node{Value: value}
 }
 
-func (t *BTree) Add(value int) {
-	t.root = add(t.root, value)
-}
-
-func (t *BTree) FindNode(value int) *node {
-	return findNode(t.root, value)
-}
-
-func (t *BTree) DeleteNode(value int) {
-	node := t.FindNode(value)
-	if node == nil {
-		return
-	}
-
-	node = nil
-}
-
-func (t *BTree) Root() *node {
-	return t.root
-}
-
-func add(current *node, value int) *node {
-	if current == nil {
-		current = newNode(value)
-		return current
-	}
-
-	if value < current.Value {
-		current.Left = add(current.Left, value)
-	} else if value > current.Value {
-		current.Right = add(current.Right, value)
-	}
-
-	return current
-}
-
-func findNode(current *node, value int) *node {
-	if current == nil {
-		return nil
-	}
-
-	if value == current.Value {
-		return current
-	}
-
-	if value < current.Value {
-		return findNode(current.Left, value)
+func (t *Node) Insert(value int) {
+	if t.Value < value {
+		// Move right
+		if t.Right == nil {
+			t.Right = &Node{Value: value}
+		} else {
+			t.Right.Insert(value)
+		}
 	} else {
-		return findNode(current.Right, value)
+		// Move left
+		if t.Left == nil {
+			t.Left = &Node{Value: value}
+		} else {
+			t.Left.Insert(value)
+		}
+	}
+}
+
+func (t *Node) Search(value int) bool {
+	if t == nil {
+		return false
+	}
+
+	if value < t.Value {
+		return t.Left.Search(value)
+	} else if value > t.Value {
+		return t.Right.Search(value)
+	} else {
+		return true
 	}
 }
